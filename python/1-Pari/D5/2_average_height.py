921f3cad8a9904f7958a9b48dc5fd908e752e9ff
shery <sh_manoochehri@yahoo.com>
2022-03-07 14:15:35 -0800

    D5

---
+++ python/1-Pari/D5/2_average_height.py
@@ -0,0 +1,41 @@
+# Instructions
+#
+# You are going to write a program that calculates the average student height from a List of heights.
+#
+# e.g. student_heights = [180, 124, 165, 173, 189, 169, 146]
+#
+# The average height can be calculated by adding all the heights together and dividing by the total number of heights.
+#
+# e.g.
+#
+# 180 + 124 + 165 + 173 + 189 + 169 + 146 = 1146
+#
+# There are a total of 7 heights in student_heights
+#
+# 1146 ÷ 7 = 163.71428571428572
+#
+# Average height rounded to the nearest whole number = 164
+#
+# Important You should not use the sum() or len() functions in your answer. You should try to replicate their
+# functionality using what you have learnt about for loops.
+
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+# Write your code below this row 👇
+# student_heights is an array first string second change to number
+print(student_heights)
+num_elem_in_student_height = 0
+sum = 0
+average = 0
+for elm in student_heights:
+    num_elem_in_student_height += 1
+    sum += elm
+average = sum / num_elem_in_student_height
+average = round(average)
+print(f" Number of item is {num_elem_in_student_height} and sum is {sum} and average is {average}")
+
---
+++ python/1-Pari/D5/4_range.py
@@ -0,0 +1,4 @@
+sum = 0
+for num in range(1, 101):
+    sum += num
+print(f"Sum of number between 1 and 100 is {sum}")
